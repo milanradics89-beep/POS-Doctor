8c33339e5bdf7e7f45c35e17369635738f18d2c6
milanradics89-beep <milan.radics89@gmail.com>
2026-09-08 20:36:54 +0200

    feat(phase4): expose public commerce layer boundary

---
+++ src/core/intelligence/phase4/index.ts
@@ -0,0 +1,8 @@
+export type {
+  ProductCatalogItem,
+  ProductCatalogQuery,
+  ProductCatalogProvider,
+} from './productCatalog';
+
+export { EmptyProductCatalogProvider } from './productCatalog';
+export { ProductCatalogCandidateProvider } from './productCatalogAdapter';
