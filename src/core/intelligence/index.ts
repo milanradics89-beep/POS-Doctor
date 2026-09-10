04b346278552dbae1b940fd11046ada837a73af6
milanradics89-beep <milan.radics89@gmail.com>
2026-09-10 10:31:46 +0200

    refactor(phase18): expose unified contract from intelligence core

--- src/core/intelligence/index.ts
+++ src/core/intelligence/index.ts
@@ -1,3 +1,5 @@
 export * from './types';
 export { mockProvider } from './mockProvider';
 export { googleProductProvider } from './googleProductProvider';
+export { USEIT_ANALYZE_CONTRACT_VERSION, validateUseitAnalyzeResponse } from './useitAnalyzeContract';
+export type { UseitAnalyzeResponse, UseitIntent, UseitSuggestion, UseitShopping, UseitShoppingCandidate } from './useitAnalyzeContract';
